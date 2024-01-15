faa60b303bf939bd4dec96c965d9211a9b58e145
Florescu Adina <florescuadina20@gmail.com>
2024-01-15 17:03:25 +0200

    first commit

---
+++ emagtest.cy.js
@@ -0,0 +1,16 @@
+describe('Enter the Emag website', () =>{
+
+it('Checking the wish list button', () =>{
+
+cy.visit('https://emag.ro');
+
+cy.get('.js-accept').click();
+cy.get('#my_cart').click();
+cy.get('#searchboxTrigger').type('headphones');
+cy.get('.searchbox-submit-button > .em').click();
+cy.get('[data-name="Casti Co2 pentru Apple iPhone, Model tip Lightning, In-Ear, Alb"] > .card-v2 > .card-v2-wrapper > .card-v2-content > .card-v2-atc > form > .btn').click();
+
+})
+
+
+})
